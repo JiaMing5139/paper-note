5ff992bab5115d18a034640dfb4a17db13262c25
jiamingpan <821269398@QQ.COM>
2019-09-23 10:05:45 +0800

    homepage

--- src/App.vue
+++ src/App.vue
@@ -8,25 +8,27 @@
       <router-link to="/sign_up">sign_up</router-link> |-->
       <el-header>
         <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-          <el-menu-item index="1">首页</el-menu-item>
+          <el-menu-item index="1">Home</el-menu-item>
 
-          <el-menu-item index="1">探索</el-menu-item>
+          <el-menu-item index="2">Discover</el-menu-item>
           <el-submenu index="2-4">
-            <template slot="title">排行</template>
+            <template slot="title">Rank</template>
             <el-menu-item index="2-4-1">选项1</el-menu-item>
             <el-menu-item index="2-4-2">选项2</el-menu-item>
             <el-menu-item index="2-4-3">选项3</el-menu-item>
           </el-submenu>
-          <el-menu-item> <el-input>
+
+          <el-menu-item >Search</el-menu-item >
+          <el-menu-item > <el-input>
             placeholder="请输入内容"
             prefix-icon="el-icon-search"
             v-model="input2">
           </el-input></el-menu-item>
 
-          <el-submenu index="2">
-            <template slot="title">用户</template>
-            <el-menu-item index="2-1">登陆</el-menu-item>
-            <el-menu-item index="2-2">注册</el-menu-item>
+          <el-submenu index="4" id ="user">
+            <template slot="title">User</template>
+            <el-menu-item index="2-1">Sign up</el-menu-item>
+            <el-menu-item index="2-2">Sign in</el-menu-item>
 
           </el-submenu>
 
@@ -56,17 +58,24 @@
     }
   }
   .el-header {
-    width:100%;
-    height: auto;
+    width:80%;
+    height:200px;
     position: absolute;
-    left:0;
+    left:200px;
     right: 0;
     top:0;
     text-align: center;
     line-height: 60px;
+
   }
   .el-input{
-    white-space: nowrap;
+    text-align:center;
+
+  }
+  #user{
+    top:0;
+    right:0;
+    position: absolute;
   }
 
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,85 @@
 <template>
   <div class="home">
+    <el-row >
+      <el-col :span="4">
+        <h5>catlog</h5>
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+          @open="handleOpen"
+          @close="handleClose">
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>computer</span>
+            </template>
+            <el-menu-item-group>
+              <template slot="title">分组一</template>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="1-4-1">选项1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>medicine</span>
+            </template>
+            <el-menu-item-group>
+              <template slot="title">分组一</template>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="1-4-1">选项1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>engineer</span>
+            </template>
+            <el-menu-item-group>
+              <template slot="title">分组一</template>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="1-4-1">选项1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
 
-    <el-container>
+        </el-menu>
+      </el-col>
+      <el-col :span="12" id = "main_view">
+        <h>paper ground</h>
+        <el-row>
+          <el-col :span="24" id="paper_display"> <div class="grid-content bg-purple-dark">paper 1</div></el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="24" id="paper_display"> <div class="grid-content bg-purple-light">paper 2</div></el-col>
+        </el-row>
+        <el-row></el-row>
+        <el-row>
+          <el-col :span="24" id="paper_display"> <div class="grid-content bg-purple-dark">paper 3</div></el-col>
+        </el-row>
+      </el-col>
 
-      <el-container>
-        <el-aside width="210px">Aside</el-aside>
-        <el-container>
-          <el-main>Main</el-main>
-          <el-footer>Footer</el-footer>
-        </el-container>
-      </el-container>
-    </el-container>
-
+    </el-row>
 
 
   </div>
@@ -19,7 +87,6 @@
 </template>
 
 <style>
-
    .el-footer {
     background-color: #B3C0D1;
     color: #333;
@@ -28,10 +95,10 @@
   }
 
   .el-aside {
-    background-color: #D3DCE6;
-    color: #333;
+
     text-align: center;
     line-height: 200px;
+    line-width: 500px;
   }
 
   .el-main {
@@ -42,7 +109,7 @@
   }
 
   body > .el-container {
-    margin-bottom: 40px;
+    margin-bottom: 200px;
   }
 
   .el-container:nth-child(5) .el-aside,
@@ -54,7 +121,31 @@
     line-height: 320px;
   }
 </style>
-
+<style>
+  #paper_display{
+    line-height: 150px;
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
+</style>
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
@@ -66,6 +157,7 @@ export default {
       isCollapse: true
     };
   },
+
   components: {
     HelloWorld
   },
