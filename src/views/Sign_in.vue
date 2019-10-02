1116e0e4877748ef3df7896e4d8103acd6cd5b83
sang <sangweixiao@163.com>
2019-10-01 20:56:10 -0500

    login signup

--- src/views/Sign_in.vue
+++ src/views/Sign_in.vue
@@ -1,11 +1,49 @@
 <template>
-<p>this Sign_in</p>
+<!--  <p>this login</p>-->
+<!--function: username and password-->
+  <div class="login">
+    <el-row :gutter="20">
+      <el-col :span="6" :offset="8">
+        <el-form ref="form" :model="form" label-width="160px">
+          <el-form-item label="Account">
+            <el-input placeholder="Input your account" v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="Password">
+            <el-input placeholder="Input your password" v-model="input2" show-password></el-input>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
+
+    <el-row :gutter="20">
+      <el-col :span="6" :offset="9">
+        <el-button>Sign in</el-button><el-button>Forget Password ?</el-button>
+      </el-col>
+    </el-row>
+
+  </div>
 </template>
 
 <script>
-  export default {
-    name: 'Sign_in'
+export default {
+  name: 'Sign_in',
+  data () {
+    return {
+      input: '',
+      input2: '',
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      }
+    }
   }
+}
 </script>
 
 <style scoped>
--- src/views/Sign_up.vue
+++ src/views/Sign_up.vue
@@ -1,11 +1,71 @@
 <template>
-  <p>this Sign_up</p>
+<!--  function: input user information-->
+  <div class="in">
+    <el-row :gutter="20">
+      <el-col :span="6" :offset="8">
+        <el-form ref="form" :model="form" label-width="160px">
+          <el-form-item label="Your First name">
+            <el-input v-model="form.firstname"></el-input>
+          </el-form-item>
+          <el-form-item label="Your Last name">
+            <el-input v-model="form.lastname"></el-input>
+          </el-form-item>
+          <el-form-item label="Account Name">
+            <el-input v-model="form.accountname"></el-input>
+          </el-form-item>
+          <el-form-item label="Password">
+            <el-input v-model="form.password1"></el-input>
+          </el-form-item>
+          <el-form-item label="Confirm Password">
+            <el-input v-model="form.password2"></el-input>
+          </el-form-item>
+          <el-form-item label="E-mail">
+            <el-input v-model="form.email"></el-input>
+          </el-form-item>
+          <el-form-item label="Birthday">
+            <el-col :span="50">
+              <el-date-picker type="date" placeholder="Choose date" v-model="form.date1" style="width: 100%;"></el-date-picker>
+            </el-col>
+          </el-form-item>
+          <el-form-item label="I have read this agreement">
+            <el-switch v-model="form.delivery"></el-switch>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="6" :offset="8">
+        <el-form ref="form" :model="form" label-width="160px">
+          <el-form-item>
+            <el-button type="primary" @click="onSubmit">I Agree</el-button>
+            <el-button>I Disagree</el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: 'Sign_up'
+export default {
+  name: 'Sign_in',
+  data () {
+    return {
+      form: {
+        firstname: '',
+        lastname: '',
+        accountname: '',
+        password1: '',
+        password2: '',
+        email: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      }
+    }
   }
+}
 </script>
 
 <style scoped>
