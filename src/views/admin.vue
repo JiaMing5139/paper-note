a032db4fadd620a2a7a3ce25e5fbdc5c368649b1
jiamingpan <821269398@QQ.COM>
2019-10-22 01:06:03 +0800

    loading

--- src/views/admin.vue
+++ src/views/admin.vue
@@ -1,12 +1,13 @@
 <template>
-  <el-tabs type="border-card" style="width: 70%;margin-left: 200px">
+  <el-tabs type="border-card" style="width: 70%;margin-left: 200px" v-loading="loading">
     <el-tab-pane label="upload paper">
       <el-upload
         class="upload-demo"
         drag
         accept = ".docx"
         action="http://127.0.0.1:5000/paper_upload"
-        on-success="onSuccess"
+        :on-success="onSuccess"
+        :on-progress="onProcess"
         multiple>
         <i class="el-icon-upload"></i>
         <div class="el-upload__text">drag files here，or<em> click to upload</em></div>
@@ -24,11 +25,16 @@
 export default {
   name: 'admin',
   data: function () {
-    return { message: 'hellow' }
+    return { message: 'hellow',
+      loading: false }
   },
   methods: {
     onSuccess (response, file, fileList) {
+      this.loading = false
       alert(file.name + 'upload success')
+    },
+    function (event, file, fileList) {
+      this.loading = true
     }
   }
 }
